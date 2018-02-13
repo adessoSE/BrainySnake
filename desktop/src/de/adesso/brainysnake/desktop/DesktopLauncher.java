59844ea224d47b954967b8c6f7ecf2eb56d28bfd
SilasMahler <simah002@stud.fh-dortmund.de>
2018-02-13 20:00:34 +0100

    remove spring boot, add logback logger

--- desktop/src/de/adesso/brainysnake/desktop/DesktopLauncher.java
+++ desktop/src/de/adesso/brainysnake/desktop/DesktopLauncher.java
@@ -8,24 +8,13 @@ import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 
 import de.adesso.brainysnake.BrainySnake;
 import de.adesso.brainysnake.Config;
-import de.adesso.brainysnake.Gamelogic.Game;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import javax.annotation.PostConstruct;
-
-@SpringBootApplication
 public class DesktopLauncher {
     private static final Logger LOGGER = LoggerFactory.getLogger(DesktopLauncher.class.getName());
 
     public static void main(String[] args) {
-        SpringApplication.run(Application.class, args);
-        loadConfigurationAnsStartApp();
-    }
-
-    private static void loadConfigurationAnsStartApp(){
         LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
 
         config.title = "BrainySnake";
@@ -36,10 +25,9 @@ public class DesktopLauncher {
         config.samples = 4;
         config.width = Config.APPLICATION_WIDTH;
         config.height = Config.APPLICATION_HEIGHT;
-        config.addIcon("./core/assets/icon.png", Files.FileType.Internal);
+        config.addIcon("icon.png", Files.FileType.Internal);
 
         new LwjglApplication(new BrainySnake(), config);
         Gdx.app.setLogLevel(Application.LOG_INFO);
-        LOGGER.info("DesktopLauncher started");
-    }
+        LOGGER.info("DesktopLauncher started");    }
 }
