c713b82127dbdf515a8287cb86a7484bf2e03777
Alexander Brockmann <brock021@freenet.de>
2018-02-13 17:00:43 +0100

    finished state, close issue
    Random Player spawn implemented

--- core/src/de/adesso/brainysnake/Gamelogic/Level/Level.java
+++ core/src/de/adesso/brainysnake/Gamelogic/Level/Level.java
@@ -3,20 +3,20 @@ package de.adesso.brainysnake.Gamelogic.Level;
 import com.badlogic.gdx.graphics.Color;
 import de.adesso.brainysnake.Config;
 import de.adesso.brainysnake.Gamelogic.Entities.GameObject;
-import de.adesso.brainysnake.Gamelogic.Game;
 import de.adesso.brainysnake.Gamelogic.Player.Snake;
 import de.adesso.brainysnake.playercommon.Orientation;
 import de.adesso.brainysnake.playercommon.math.Point2D;
+import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Random;
-import java.util.concurrent.ThreadLocalRandom;
-import java.util.logging.Logger;
 
 public class Level {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(Level.class.getName());
 
     private int width, height;
 
@@ -38,7 +38,9 @@ public class Level {
         points = new GameObject(null, Config.POINT_COLLOR);
     }
 
-    /**returns a LinkedList with all Positions all Barriers*/
+    /**
+     * returns a LinkedList with all Positions all Barriers
+     */
     private LinkedList<Point2D> buildBarriers() {
         LinkedList<Point2D> barriers = new LinkedList<Point2D>();
         int gapBarriersX = width / 4;
@@ -53,7 +55,9 @@ public class Level {
         return barriers;
     }
 
-    /**returns a LinkedList with the positions of the generated Barrier*/
+    /**
+     * returns a LinkedList with the positions of the generated Barrier
+     */
     private List<Point2D> addBarrier(int x, int y) {
         List<Point2D> barrierDots = new ArrayList<Point2D>();
 
@@ -71,7 +75,9 @@ public class Level {
         return barrierDots;
     }
 
-    /**returns a LinkedList with the positions of the generated LevelWalls*/
+    /**
+     * returns a LinkedList with the positions of the generated LevelWalls
+     */
     private LinkedList<Point2D> buildOuterWalls() {
         LinkedList<Point2D> positionts = new LinkedList<Point2D>();
         for (int x = 0; x < width; x++) {
@@ -119,12 +125,16 @@ public class Level {
         return points;
     }
 
-    /**checks if the given point collides with an already existing point*/
+    /**
+     * checks if the given point collides with an already existing point
+     */
     public boolean checkCollision(Point2D point2D) {
         return levelWalls.getPositions().contains(point2D) || barriers.getPositions().contains(point2D);
     }
 
-    /**creates a new Snake and returns the new Snake*/
+    /**
+     * creates a new Snake and returns the new Snake
+     */
     public Snake createStartingGameObject(int initialLength) {
         LinkedList<Point2D> head = new LinkedList<Point2D>();
         LinkedList<Point2D> body = new LinkedList<Point2D>();
@@ -132,53 +142,58 @@ public class Level {
         Orientation orientation = getRandomOrientation();
         int centerX = start.getX();
         int centerY = start.getY();
-        for (int i = 1; i <= initialLength; i++) {
+        head.add(start);
+        for (int i = 1; i < initialLength; i++) {
             Point2D positionIn = getPositionIn(orientation, centerX, centerY, i);
-            if (i == initialLength) {
-                head.addFirst(positionIn);
-            } else {
-                body.addFirst(positionIn);
-            }
+            body.add(positionIn);
+            LOGGER.info("BodySpawn - X:" + positionIn.getX() + " Y:" + positionIn.getY() + " Orientation:" + orientation);
         }
         snakesStartingPositions.addAll(head);
         snakesStartingPositions.addAll(body);
         return new Snake(new GameObject(head), new GameObject(body), orientation);
     }
 
+    /**
+     * returns a rendom Orientation
+     */
     public Orientation getRandomOrientation() {
         Random random = new Random();
         int randomNumber = random.nextInt(4);
-        switch(randomNumber){
-            case 0 :
+        switch (randomNumber) {
+            case 0:
                 return Orientation.UP;
-            case 1 :
+            case 1:
                 return Orientation.DOWN;
-            case 2 :
+            case 2:
                 return Orientation.LEFT;
-            case 3 :
+            case 3:
                 return Orientation.RIGHT;
             default:
                 return Orientation.RIGHT;
         }
     }
 
-    /**returns the next position depending on the Orientation*/
+    /**
+     * returns the position behind the given position depending on the Orientation and the given length
+     */
     private Point2D getPositionIn(Orientation orientation, int centerX, int centerY, int length) {
         switch (orientation) {
             case UP:
-                return new Point2D(centerX, centerY + length);
-            case DOWN:
                 return new Point2D(centerX, centerY - length);
+            case DOWN:
+                return new Point2D(centerX, centerY + length);
             case RIGHT:
-                return new Point2D(centerX + length, centerY);
-            case LEFT:
                 return new Point2D(centerX - length, centerY);
+            case LEFT:
+                return new Point2D(centerX + length, centerY);
             default:
                 return null;
         }
     }
 
-    /**spreads the consumable Points*/
+    /**
+     * spreads the consumable Points
+     */
     public void spreadPoints() {
         for (int i = 0; i < (maxPointsInLevel - points.size()); i++) {
             Point2D randomLevelPosition = null;
@@ -225,7 +240,9 @@ public class Level {
         return false;
     }
 
-    /**checks if the given point is within the level*/
+    /**
+     * checks if the given point is within the level
+     */
     public boolean levelContainsPosition(Point2D point2D) {
         return point2D.x >= 0 && point2D.y >= 0 && point2D.x < width && point2D.y < height;
     }
@@ -242,7 +259,8 @@ public class Level {
         try {
             for (int x = snakeHeadx - snakeLength; x < snakeHeadx + snakeLength; x++) {
                 for (int y = snakeHeady - snakeLength; y < snakeHeady + snakeLength; y++) {
-                    if (checkCollision(new Point2D(x, y))&&!snakesStartingPositions.contains(new Point2D(x,y))) {
+                    if (checkCollision(new Point2D(x, y)) || snakesStartingPositions.contains(new Point2D(x, y))) {
+                        LOGGER.error("Colission at - X:" + x + " Y:" + y);
                         return false;
                     }
                 }
@@ -251,6 +269,7 @@ public class Level {
             System.out.println("IndexOutOfBoundsException");
             return false;
         }
+        LOGGER.info("Valid position at - X:" + snakeHeadx + " Y:" + snakeHeady);
         return true;
     }
 
