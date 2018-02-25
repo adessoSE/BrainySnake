499d2a73401663a2e21e4b73a892924e69ba693f
Rudi Klassen <rudolfklassen2@gmail.com>
2018-02-25 16:59:02 +0100

    Changed Logo and Backgroundcolor von Screens

--- core/src/de/adesso/brainysnake/screenmanagement/screens/AbstractScreen.java
+++ core/src/de/adesso/brainysnake/screenmanagement/screens/AbstractScreen.java
@@ -51,7 +51,7 @@ public abstract class AbstractScreen extends Stage implements Screen {
     @Override
     public void render(float delta) {
         // Clear screen
-        Gdx.gl.glClearColor(1, 1, 1, 1);
+        Gdx.gl.glClearColor(0, 0, 0, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 
         // Calling to Stage methods
